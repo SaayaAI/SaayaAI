711389b5466f0c2154b277c772d9a997a7a4acf8
Shreeram <shreeram@Shreerams-MacBook-Air.local>
2026-06-15 00:00:23 +0530

    Fixed port issue

--- bot.py
+++ bot.py
@@ -54,5 +54,5 @@ def webhook():
         return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    
+    port = int(os.environ.get("PORT", 10000)) # Render PORT variable use karega
+    app.run(host='0.0.0.0', port=port)
