8c5c535ab92d9d43661d27f8f86e0bb2ce600bd9
SaayaAI <saaya.ai.official@gmail.com>
2026-06-23 22:55:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -416,3 +416,8 @@ def webhook():
         print("Webhook Error:", str(e))
 
         return "ok", 200
+        import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
